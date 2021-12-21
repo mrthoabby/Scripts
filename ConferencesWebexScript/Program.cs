e9ee0426ebc5f13308c388ea18076e46eaa4f43c
Danis Abadia <d.abadia@desarrollo51>
2021-12-21 18:20:59 -0500

    Verificando que ya aparezcan mis credenciales

--- ConferencesWebexScript/Program.cs
+++ ConferencesWebexScript/Program.cs
@@ -1,28 +1,50 @@
 ﻿using ConferencesWebexScript.ApiTemplates;
 using ConferencesWebexScript.Entities;
+using ConferencesWebexScript.Enums;
 using ConferencesWebexScript.Helpers;
 using ConferencesWebexScript.Services;
 using System;
+using System.Threading.Tasks;
 
 namespace ConferencesWebexScript
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static async Task Main(string[] args)
         {
+
             //Se le adiciona 1 minuto a la tarea para que no se pisen las mangueras
-            var CurrentTime = DateTime.Now.AddMinutes(1);
+            DateTime CurrentTime = DateTime.Now.AddMinutes(1);
+            ConferenceRequestCreate _createConferenceTemplate = new ConferenceRequestCreate()
+            {
+                Event = Enum.GetName(EnumEvents.start),
+                Data_create_update = ScriptConfigurationHelper.GetConferenceParameter(),
+            };
+            _createConferenceTemplate.Data_create_update.Start_time = CurrentTime.ToString("s");
+            _createConferenceTemplate.Data_create_update.End_time = CurrentTime.AddHours(12).ToString("s");
+            ConferenceRequestRecording _recordConeferenceTemplate = new ConferenceRequestRecording()
+            {
+                Event = Enum.GetName(EnumEvents.set_recording),
+                Data_Recording = new DataRecording()
+                {
+                    meetingId = ScriptConfigurationHelper._meetingId,
+                    recordingStarted = true,
+                }
+            };
 
-            var conferencingParameters = ScriptConfigurationHelper.GetConferenceParameter();
 
-            if (conferencingParameters is null) throw new Exception("No se han configurado de manera correcta los parametros para iniciar la conferencia");
 
-            conferencingParameters.Start_time = CurrentTime.ToString("s");
-            conferencingParameters.End_time = CurrentTime.AddHours(12).ToString("s");
+            (bool IsConferenceRecording, ConferenceResponseRecording ConferenceContent) _conference = await Conference.StartRecordingAsync(_recordConeferenceTemplate);
+            if (_conference.IsConferenceRecording)
+            {
+                ConferenceResponseRecording result = _conference.ConferenceContent;
+                if(!string.IsNullOrEmpty(result.message)) Console.WriteLine( $"Error al intentar grabar sesión: {result.message}" );
+            }
 
-            var _conference = Conference.CreateConferenceAsync(new ConferenceRequestCreate() { Data_create_update = conferencingParameters }).Result;
-            if (_conference.IsConferenceCreate) Console.WriteLine(_conference.ConferenceContent.title);
             Console.ReadKey();
         }
+
+
+
     }
 }
